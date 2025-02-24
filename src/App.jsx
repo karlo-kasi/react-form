99b8f1cf1bb9542faf6e256c56045c835b787098
karlo-kasi <karlokasi1995@gmail.com>
2025-02-24 21:23:01 +0100

    Add: milestone 1 + milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
 import { useState } from 'react'
 import './App.css'
+import initialArticles from "./data/articles"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [articles, setArticle] = useState(initialArticles);
+
+  const [newArticle, setNewArticle] = useState("");
+
+  const addArticle = (e) => {
+    e.preventDefault()
+    const article = newArticle.trim()
+    setArticle([...articles, article])
+    setNewArticle("")
+
+  }
+
+  const removeArticle = (indiceElementoArray) => {
+
+    const arrayClone = articles.filter((_, index) => index !== indiceElementoArray)
+    return setArticle(arrayClone)
+
+  }
 
   return (
     <>
-      
+      <div className="container mt-5">
+        <h1>Lista Articoli</h1>
+        <ul className="list-group">
+          {articles.map((element, index) => {
+            return (
+              <li key={index} className='list-group-item'>
+                {element}
+
+                <button
+                  className="btn btn-danger btn-sm float-end"
+                  onClick={() => removeArticle(index)}
+                >
+                  X
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+
+        {/* //form per il nuovo items  */}
+        <form onSubmit={addArticle}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Aggiungi il titolo dell'articolo?"
+            value={newArticle}
+            onChange={(e) => setNewArticle(e.target.value)}
+          />
+
+          <button className="btn btn-primary mt-3">Aggiungi</button>
+        </form>
+      </div>
+
     </>
   )
 }
---
+++ src/data/articles.js
@@ -0,0 +1,14 @@
+const initialArticles = [
+    "Laptop Dell XPS 13", 
+    "Nike Air Force 1", 
+    "Smartphone Samsung Galaxy S23", 
+    "Orologio Apple Watch Series 8", 
+    "Zaino North Face Borealis", 
+    "Chitarra Fender Stratocaster", 
+    "Tastiera meccanica Logitech G Pro", 
+    "Monitor LG UltraWide 34''",
+    "Libro 'Atomic Habits' - James Clear",
+    "Macchina del caffè Nespresso"
+]
+
+export default initialArticles;
